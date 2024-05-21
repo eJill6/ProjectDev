327304cee84f7093ae8b548719fa6a50190dd3fe
Jill <Jill.Jhang@ark88.net>
2024-05-21 17:46:10 +0800

    ADD

---
+++ Work/WK/MS/Lib/JxBackendServiceLibrary/UnitTestN6/CacheTest/ConcurrentLockTest.cs
@@ -0,0 +1,89 @@
+﻿using Autofac;
+using JxBackendService.Common.Util.Cache;
+using JxBackendService.Model.Enums;
+using JxBackendServiceN6.Common.Util;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using UnitTestN6;
+
+namespace UnitTest.CacheTest
+{
+    [TestClass]
+    public class ConcurrentLockTest : BaseUnitTest
+    {
+        private static readonly object s_cacheWriteLocker = new object();
+
+        private static readonly string s_lockerKeyFormat = "@#$Locker_{0}";
+
+        protected override void AppendServiceToContainerBuilder(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<UnitTestMobileApiEnvironmentService>().AsImplementedInterfaces();
+        }
+
+        private const int taskCount = 10000;
+
+        private readonly Task[] tasks = new Task[taskCount];
+
+        [TestMethod]
+        public void GetLockTest()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            //第一次沒有快取，會進入new object並且放入memory cache
+            DoGetLocker();
+            stopwatch.Stop();
+
+            //等待上方log寫入,避免花費時間的log混雜在中間
+            Task.Delay(2000).Wait();
+            LogUtil.ForcedDebug($"ElapsedMilliseconds:{stopwatch.ElapsedMilliseconds}");
+
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            //第二次會直接拿到memory cache中的lock
+            DoGetLocker();
+            stopwatch.Stop();
+
+            //等待上方log寫入,避免花費時間的log混雜在中間
+            Task.Delay(2000).Wait();
+            LogUtil.ForcedDebug($"ElapsedMilliseconds:{stopwatch.ElapsedMilliseconds}");
+        }
+
+        private void DoGetLocker()
+        {
+            for (int i = 0; i < taskCount; i++)
+            {
+                string key = string.Format(s_lockerKeyFormat, i);
+
+                tasks[i] = Task.Run(() =>
+                {
+                    GetLocker(key);
+                    LogUtil.ForcedDebug($"{DateTime.Now}, {key}");
+                });
+            }
+        }
+
+        private static object GetLocker(string key)
+        {
+            string lockerCacheKey = string.Format(s_lockerKeyFormat, key);
+            object locker = null;
+
+            lock (s_cacheWriteLocker)
+            {
+                locker = MemoryCacheUtil.GetCache(
+                    lockerCacheKey,
+                    isCloneInstance: false,
+                    isForceRefresh: false,
+                    cacheSeconds: 300,
+                    isSlidingExpiration: true,
+                    getCacheData: () =>
+                    {
+                        return new object();
+                    });
+            }
+
+            return locker;
+        }
+    }
+}
---
+++ Work/WK/MS/Lib/JxBackendServiceLibrary/UnitTestN6/ChatTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using JxBackendService.DependencyInjection;
+using JxBackendService.Interface.Service.Chat;
+using JxBackendService.Model.Enums;
+using JxBackendService.Model.Enums.Chat;
+using JxBackendService.Model.Param.Chat;
+using JxBackendService.Common.Util;
+using Autofac;
+
+namespace UnitTestN6
+{
+    [TestClass]
+    public class ChatTest : BaseUnitTest
+    {
+        private readonly Lazy<IIdGeneratorService> _idGeneratorService;
+
+        private readonly Lazy<IChatMessageService> _chatMessageService;
+
+        protected override void AppendServiceToContainerBuilder(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<UnitTestBatchEnvironmentService>().AsImplementedInterfaces();
+        }
+
+        private readonly List<int> randomUserIDs = new List<int>() { 888, 2384, 4646, 6251 };
+
+        public ChatTest()
+        {
+            _idGeneratorService = DependencyUtil.ResolveJxBackendService<IIdGeneratorService>(EnvironmentUser, DbConnectionTypes.Master);
+            _chatMessageService = DependencyUtil.ResolveJxBackendService<IChatMessageService>(EnvironmentUser, DbConnectionTypes.IMsgMaster);
+        }
+
+        [TestMethod]
+        public void CreateChatMessage()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                int randomIndex = new Random().Next(0, randomUserIDs.Count);
+
+                var param = new SendAddMessageQueueParam()
+                {
+                    OwnerUserID = 2607,
+                    Message = $"hello{i}_{randomUserIDs[randomIndex]}",
+                    MessageTypeValue = MessageType.Text.Value,
+                    MessageID = _idGeneratorService.Value.CreateId(),
+                    PublishUserID = randomUserIDs[randomIndex],
+                    RoomID = randomUserIDs[randomIndex].ToString(),
+                    PublishTimestamp = DateTime.UtcNow.ToUnixOfTime()
+                };
+
+                _chatMessageService.Value.AddChatMessage(param);
+            }
+        }
+    }
+}
